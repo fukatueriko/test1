52fe6131d604e4c359e6d4a3fec3ccc14ec1ffde
fukatueriko <sunkistpeach982000@yahoo.co.jp>
2014-11-17 18:46:20 +0900

    submit(3)

--- submit2.py
+++ submit2.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import Imputer # 欠損値を埋める
-from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 print u'訓練データ読み込み中'
 df=pd.read_csv("C:\\Users\\fukazu\\Documents\\IPython Notebooks\\deepanAlytics\\train.csv",header=None)
@@ -25,33 +25,35 @@ vectorizer = DictVectorizer(sparse=True)
 x = vectorizer.fit_transform(x.to_dict('records'))
 
 print u'欠損値を埋める'
-inp = Imputer()
-x = inp.fit_transform(x)
+imp = Imputer()
+x = imp.fit_transform(x)
 
 print u'特徴ベクトル生成中'
+N = x.shape[0] # 訓練データ数
+sel = np.random.choice(range(N), N/100) # ランダムに訓練データの番号を選ぶ
+
 gbdt = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, min_samples_split=2, min_samples_leaf=1, max_depth=7, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False)
-gbdt.fit(x,toarray(),y)
+gbdt.fit(x[sel].toarray(), y[sel])
 x_new = gbdt.transform(x)
 
-clf = SVC()
-clf.fit(x_new, y)
+clf = RandomForestClassifier()
+clf.fit(x_new[sel].toarray(), y[sel])
 
 print u'テストデータ読み込み中'
 df=pd.read_csv("C:\\Users\\fukazu\\Documents\\IPython Notebooks\\deepanAlytics\\test.csv",header=None)
 
 x = df.loc[:, 1:]
-
 x.loc[:, 4:9] = x.loc[:, 4:9].astype(str)
 
 print u'数量化中'
 x = vectorizer.transform(x.to_dict('records'))
 
 print u'欠損値を埋める'
-x = inp.transform(x)
+x = imp.transform(x)
 
 print u'予測中'
 x_new = gbdt.transform(x)
-answer = clf.predict(x_new)
+answer = clf.predict_proba(x_new.toarray())[:, 1]
 
 print u'サブミットファイル生成'
 index = df[0]
